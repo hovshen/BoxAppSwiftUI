03664aa3ad10289bf2b771b6df6787d49642c6a3
q <q>
2025-10-22 14:39:59 +0800

    Initial Commit

---
+++ BoxAppSwiftUI/BoxAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxAppSwiftUIApp.swift
+//  BoxAppSwiftUI
+//
+//  Created by Ho Shen Jui on 2025/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BoxAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoxAppSwiftUITests/BoxAppSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  BoxAppSwiftUITests.swift
+//  BoxAppSwiftUITests
+//
+//  Created by Ho Shen Jui on 2025/10/22.
+//
+
+import Testing
+@testable import BoxAppSwiftUI
+
+struct BoxAppSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
